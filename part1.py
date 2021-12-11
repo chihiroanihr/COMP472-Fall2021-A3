b170d394e5451df34983f46f6cea741c2b9875b7
Rhina Kim <chihiroanihr@gmail.com>
2021-12-11 10:31:04 -0500

    init

--- part1.py
+++ part1.py
@@ -1,3 +1,6 @@
 import gensim.downloader as api
+#model = api.load("glove-wiki-gigaword-100")
 
-model = api.load("glove-wiki-gigaword-100")
+#https://github.com/RaRe-Technologies/gensim-data
+#https://rare-technologies.com/new-download-api-for-pretrained-nlp-models-and-datasets-in-gensim/
+#https://radimrehurek.com/gensim/
