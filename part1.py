2cd210f2ef190861ef27ef5bca632017f57fc660
Rhina Kim <chihiroanihr@gmail.com>
2021-12-12 07:15:54 -0500

    modify part2

--- part1.py
+++
@@ -1,6 +0,0 @@
-import gensim.downloader as api
-import pandas as pd
-from IPython.core.display import HTML
-#info = api.info()  # show info about available models/datasets
-#print(info)
-data = pd.read_csv('synonyms.csv', sep=",",header=0)
